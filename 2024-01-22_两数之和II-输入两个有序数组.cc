aa71abfaa6130a58b14f38877406d6571e278c97
lzyznl <2941649503@qq.com>
2024-01-22 19:05:34 +0800

    每日刷题

---
+++ 2024-01-22_两数之和II-输入两个有序数组.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/**
+ * 个人题解地址 https://leetcode.cn/problems/kLl5u1/solutions/2615378/liang-shu-zhi-he-ii-shu-ru-you-xu-shu-zu-xwe5/
+*/
+vector<int> twoSum(vector<int> &numbers, int target)
+{
+    vector<int> ans;
+    int length = numbers.size();
+    int left=0 ;
+    int right = length-1;
+    while(left<=right){
+        int sum = numbers[left]+numbers[right];
+        if(sum>target){
+            right--;
+        }else if(sum<target){
+            left++;
+        }else{
+            ans.push_back(left);
+            ans.push_back(right);
+            break;
+        }
+    }
+    return ans;
+}
