dcfa5605a23f0eb35a82b505d9c2baff1aaed5f2
lzyznl <2941649503@qq.com>
2024-01-23 21:36:45 +0800

    2023-01-23每日一题

---
+++ 2024-01-23_盛最多水的容器.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+/**
+ * author:lzy
+ * 个人题解：https://leetcode.cn/problems/container-with-most-water/solutions/2617174/sheng-zui-duo-shui-de-rong-qi-by-elastic-bpzn/
+*/
+int maxArea(vector<int> &height)
+{
+    int length = height.size();
+    int left = 0;
+    int right = length-1;
+    int ans = -1;
+    while(left<right){
+        ans = max(ans,(right-left)*min(height[right],height[left]));
+        if(height[left]<=height[right]){
+            left++;
+        }else{
+            right--;
+        }
+    }
+    return ans;
+}
