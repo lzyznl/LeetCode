5df0522b7b6b7340fee60bf8ad4a13e336a58df2
lzyznl <2941649503@qq.com>
2024-02-11 22:44:53 +0800

    2024-02-11

---
+++ 2024-02-11_复习二叉树的前序遍历(非递归版).cc
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+vector<int> preorderTraversal(TreeNode *root)
+{
+    stack<TreeNode*> stack;
+    vector<int> vt;
+    if(root==nullptr){
+        return vt;
+    }
+    stack.push(root);
+    while(!stack.empty()){
+        TreeNode* node = stack.top();
+        stack.pop();
+        vt.push_back(node->val);
+        if(node->right){
+            stack.push(node->right);
+        }
+        if(node->left){
+            stack.push(node->left);
+        }
+    }
+    return vt;
+}
+
+vector<int> postorderTraversal(TreeNode *root)
+{
+    //根左右->根右左->左根右
+    stack<TreeNode*> stack;
+    vector<int> vt;
+    if(root==nullptr){
+        return vt;
+    }
+    stack.push(root);
+    while(!stack.empty()){
+        TreeNode* node = stack.top();
+        stack.pop();
+        vt.push_back(node->val);
+        if(node->left){
+            stack.push(node->left);
+        }
+        if(node->right){
+            stack.push(node->right);
+        }
+    }
+    reverse(vt.begin(),vt.end());
+    return vt;
+}
+
+
+vector<int> inorderTraversal(TreeNode *root)
+{
+    vector<int> vt;
+    stack<TreeNode*> stack;
+    if(root==nullptr){
+        return vt;
+    }
+    while(root!=nullptr||!stack.empty()){
+        while(root){
+            stack.push(root);
+            root=root->left;
+        }
+        root=stack.top();
+        stack.pop();
+        vt.push_back(root->val);
+        root=root->right;
+    }
+    return vt;
+}
---
+++ 2024-02-11_复习二叉树的最近公共祖先.cc
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <stack>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+unordered_map<int,TreeNode*> parentNodeMap;
+unordered_map<int,bool> isVisited;
+void dfs(TreeNode* root){
+    if(root==nullptr){
+        return ;
+    }
+    if(root->left){
+        parentNodeMap[root->left->val]=root;
+    }
+    if(root->right){
+        parentNodeMap[root->right->val]=root;
+    }
+    dfs(root->left);
+    dfs(root->right);
+}
+
+TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q)
+{
+    if(root==nullptr){
+        return nullptr;
+    }
+    if(root==q){
+        return q;
+    }
+    if(root==p){
+        return p;
+    }
+    parentNodeMap[root->val]=nullptr;
+    dfs(root);
+    while(p){
+        isVisited[p->val]=true;
+        p=parentNodeMap[p->val];
+    }
+    while(q){
+        if(isVisited[q->val]){
+            return q;
+        }
+        q=parentNodeMap[q->val];
+    }
+    return nullptr;
+}
